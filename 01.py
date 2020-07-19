7ed90f3ad70084cf79fb1b8bd9c19b51160c3473
lvhaohui <lvhaohui@163.com>
2020-07-19 11:03:20 +0800

    gghg

--- 01.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "http://192.168.1.160:5000/login"
-data = {"username": "lucy", "password": "123456"}
-headers = {"Content-Type": "application/json"}
-
-r = requests.post(url, json=data, headers=headers)
-
-print(r.headers["Set-Cookie"])
